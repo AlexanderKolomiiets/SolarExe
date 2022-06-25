a6cff83ef03731732d1ac53f069a1fadcfdde29c
Sasha Kolomiiets <kinbean444@gmail.com>
2022-06-25 14:36:17 +0300

    upd media

--- js/main.js
+++ js/main.js
@@ -19,21 +19,21 @@ $(function(){
 
   });
 
-  $(".footer_bid").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
+  // $(".footer_bid").submit(function() { //Change
+	// 	var th = $(this);
+	// 	$.ajax({
+	// 		type: "POST",
+	// 		url: "mail.php", //Change
+	// 		data: th.serialize()
+	// 	}).done(function() {
+	// 		alert("Thank you!");
+	// 		setTimeout(function() {
+	// 			// Done Functions
+	// 			th.trigger("reset");
+	// 		}, 1000);
+	// 	});
+	// 	return false;
+	// });
    
   
     
